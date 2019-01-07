4edb9360d49373c801ae10f466bdf58be7ed233a
Edwin Cloud <ecloud412@gmail.com>
2019-01-07 14:50:19 -0800

    Add echo server to server file

--- server/main.go
+++ server/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
 
 func main() {
-	fmt.Println("Hello")
+
+	// Create new instance of echo
+	e := echo.New()
+
+	// test route
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello")
+	})
+
+	// Start http server, wrap with fatal helper func
+	e.Logger.Fatal(e.Start(":5000"))
+
 }
